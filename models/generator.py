3599ce13bc28265f9e3c6e0b5f90123e6e564630
taoran <taoranji@gmail.com>
2021-05-05 14:42:45 -0400

    fix GAN generator bug (add Tanh) and add CGAN generators

--- models/generator.py
+++ models/generator.py
@@ -1,10 +1,14 @@
 # =============================================================================
 # ================================== Import ===================================
 # =============================================================================
+import torch
 import torch.nn as nn
 
 
-class InfoGan1C28G(nn.Module):
+# =============================================================================
+# ================== Generator for 1 channel 28 x 28 images ===================
+# =============================================================================
+class G_InfoGan_1C28(nn.Module):
     """Generator used in InfoGAN paper for MNIST dataset. Can be also applied
     to Fashion-MNIST dataset.
 
@@ -13,11 +17,11 @@ class InfoGan1C28G(nn.Module):
     z_dim : int
         Dimension for the noise.
     img_channels : int
-        # of in_channels of the images.
+        # of channels of the images.
 
     """
     def __init__(self, z_dim, img_channels):
-        super(InfoGan1C28G, self).__init__()
+        super(G_InfoGan_1C28, self).__init__()
 
         # fc 1: (batch_size, z_dim -> 1024)
         self.fc1 = nn.Sequential(
@@ -36,10 +40,10 @@ class InfoGan1C28G(nn.Module):
                 nn.BatchNorm2d(64),
                 nn.ReLU())
         # un-conv 2: (batch_size, 128 -> 1, 14 -> 28, 14 -> 28)
-        self.dconv2 = nn.ConvTranspose2d(in_channels=64,
-                                         out_channels=img_channels,
-                                         kernel_size=[4, 4], stride=2,
-                                         padding=1)
+        self.dconv2 = nn.Sequential(
+                nn.ConvTranspose2d(in_channels=64, out_channels=img_channels,
+                                   kernel_size=[4, 4], stride=2, padding=1),
+                nn.Tanh())  # Tanh [-1, 1] range of transformed images
 
     def forward(self, x):
         """Forward propagation.
@@ -64,7 +68,51 @@ class InfoGan1C28G(nn.Module):
         return x
 
 
-class InfoGan3C32G(nn.Module):
+class G_InfoGan_CGAN_1C28(G_InfoGan_1C28):
+    """A CGAN compatible generator.
+
+    Only need to add condition tensor to the first layer of the G_InfoGAN_1C28.
+
+    Parameters
+    ----------
+    z_dim : int
+        Dimension for the noise.
+    img_channels : int
+        # of in_channels of the images.
+    c_dim : int
+        Condition dimension.
+
+    """
+    def __init__(self, z_dim, img_channels, c_dim):
+        super(G_InfoGan_CGAN_1C28, self).__init__(z_dim, img_channels)
+
+        # fc 1: (batch_size, z_dim + c_dim -> 1024)
+        self.fc1 = nn.Sequential(
+                nn.Linear(z_dim + c_dim, 1024),
+                nn.BatchNorm1d(1024),
+                nn.ReLU())
+
+    def forward(self, x, c):
+        """Forward propagation.
+
+        Parameters
+        ----------
+        x : :class:`torch.Tensor`
+            Sampled noise, a tensor of shape (batch_size, z_dim).
+
+        Returns
+        -------
+        x : :class:`torch.Tensor`
+            Generated figures, a tensor of shape (batch_size, 1, 28, 28).
+
+        """
+
+        x_ = torch.cat([x, c], 1)  # concat on z_dim
+        x_ = super().forward(x_)
+        return x_
+
+
+class G_InfoGan_3C32(nn.Module):
     """Generator used in InfoGAN paper for SVHN dataset.
 
     Parameters
@@ -76,7 +124,7 @@ class InfoGan3C32G(nn.Module):
 
     """
     def __init__(self, z_dim, img_channels):
-        super(InfoGan3C32G, self).__init__()
+        super(G_InfoGan_3C32, self).__init__()
 
         # fc 1: (batch_size, z_dim -> 2 * 2 * 448)
         self.fc1 = nn.Sequential(
@@ -127,3 +175,28 @@ class InfoGan3C32G(nn.Module):
         x = self.dconv3(x)
         x = self.dconv4(x)
         return x
+
+
+class G_InfoGan_CGAN_3C32(G_InfoGan_3C32):
+    """A CGAN compatible generator.
+
+    Only need to add condition tensor to the first layer of the G_InfoGAN_3C32.
+
+    Parameters
+    ----------
+    z_dim : int
+        Dimension for the noise.
+    img_channels : int
+        # of in_channels of the images.
+    c_dim : int
+        Condition dimension.
+
+    """
+    def __init__(self, z_dim, img_channels, c_dim):
+        super(G_InfoGan_CGAN_3C32, self).__init__(z_dim, img_channels)
+
+        # fc 1: (batch_size, z_dim + condition_dim -> 2 * 2 * 448)
+        self.fc1 = nn.Sequential(
+                nn.Linear(z_dim + c_dim, 2 * 2 * 448),
+                nn.BatchNorm1d(2 * 2 * 448),
+                nn.ReLU())
