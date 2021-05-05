00fefee15da591c49f707a36c877eb2270c95453
taoran <taoranji@gmail.com>
2021-05-05 18:17:02 -0400

    change dataset sequence

--- learn/data.py
+++ learn/data.py
@@ -54,6 +54,15 @@ def dataloader(batch_size, dataset_name, datapath='data'):
                 thv.datasets.FashionMNIST(datapath, train=False, download=True,
                                           transform=transform1c),
                 batch_size=batch_size, shuffle=True, drop_last=True)
+    elif dataset_name == 'cifar10':  # 10-class image recognition, 3 channels
+        tr_set = DataLoader(
+                thv.datasets.CIFAR10(datapath, train=True, download=True,
+                                     transform=transform3c),
+                batch_size=batch_size, shuffle=True, drop_last=True)
+        te_set = DataLoader(
+                thv.datasets.CIFAR10(datapath, train=False, download=True,
+                                     transform=transform3c),
+                batch_size=batch_size, shuffle=True, drop_last=True)
     elif dataset_name == 'svhn':  # object (numbers) recognition, 3 channels
         tr_set = DataLoader(
                 thv.datasets.SVHN(os.path.join(datapath, 'SVHN'),
@@ -72,15 +81,6 @@ def dataloader(batch_size, dataset_name, datapath='data'):
         te_set = DataLoader(
                 thv.datasets.CelebA(datapath, split='test', download=True,
                                     transform=transform3c),
-                batch_size=batch_size, shuffle=True, drop_last=True)
-    elif dataset_name == 'cifar10':  # 10-class image recognition, 3 channels
-        tr_set = DataLoader(
-                thv.datasets.CIFAR10(datapath, train=True, download=True,
-                                     transform=transform3c),
-                batch_size=batch_size, shuffle=True, drop_last=True)
-        te_set = DataLoader(
-                thv.datasets.CIFAR10(datapath, train=False, download=True,
-                                     transform=transform3c),
                 batch_size=batch_size, shuffle=True, drop_last=True)
     elif dataset_name == 'stl10':  # 10-class image recognition, 3 channels
         tr_set = DataLoader(
