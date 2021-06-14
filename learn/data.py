d3f902adffd48f8e807bbbab3524fc14ba317455
taoran <taoranji@gmail.com>
2021-06-14 13:44:06 -0400

    update data preprocess and add celeba dataset

--- learn/data.py
+++ learn/data.py
@@ -2,15 +2,17 @@
 # ================================== Import ===================================
 # =============================================================================
 import os
+import torch
 import torchvision as thv
 from torchvision import transforms
+import torchvision.datasets as dset
 from torch.utils.data import DataLoader
 
 
 # =============================================================================
 # ================================= Dataset ===================================
 # =============================================================================
-def dataloader(batch_size, dataset_name, datapath='data'):
+def dataloader(batch_size, dataset_name, args):
     """Return the dataloader for selected datset.
 
     Parameters
@@ -19,8 +21,6 @@ def dataloader(batch_size, dataset_name, datapath='data'):
         Minibatch size.
     dataset_name : str
         Name of the selected dataset.
-    datapath : str
-        Where to put the dataset.
 
     Returns
     -------
@@ -31,11 +31,25 @@ def dataloader(batch_size, dataset_name, datapath='data'):
 
     """
 
-    transform3c = transforms.Compose([transforms.ToTensor(),
-                                     transforms.Normalize((.5, .5, .5),
-                                                          (.5, .5, .5))])
-    transform1c = transforms.Compose([transforms.ToTensor(),
-                                     transforms.Normalize((.5), (.5))])
+    # resize images or not
+    if args.img_resize:
+        transform3c = transforms.Compose([
+            transforms.Resize(args.img_size),
+            transforms.CenterCrop(args.img_size),
+            transforms.ToTensor(),
+            transforms.Normalize((.5, .5, .5), (.5, .5, .5))])
+        transform1c = transforms.Compose([
+            transforms.Resize(args.img_size),
+            transforms.CenterCrop(args.img_size),
+            transforms.ToTensor(), transforms.Normalize((.5), (.5))])
+    else:
+        transform3c = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize((.5, .5, .5),
+                                                              (.5, .5, .5))])
+        transform1c = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize((.5), (.5))])
+    # create dataloaders
+    datapath = 'data'
     if dataset_name == 'mnist':  # handwritten digits, 1 channel
         tr_set = DataLoader(
                 thv.datasets.MNIST(datapath, train=True, download=True,
@@ -74,14 +88,19 @@ def dataloader(batch_size, dataset_name, datapath='data'):
                                   download=True, transform=transform3c),
                 batch_size=batch_size, shuffle=True, drop_last=True)
     elif dataset_name == 'celeba':  # celebrity face, 3 channel
-        tr_set = DataLoader(
-                thv.datasets.CelebA(datapath, split='train', download=True,
-                                    transform=transform3c),
-                batch_size=batch_size, shuffle=True, drop_last=True)
-        te_set = DataLoader(
-                thv.datasets.CelebA(datapath, split='test', download=True,
-                                    transform=transform3c),
-                batch_size=batch_size, shuffle=True, drop_last=True)
+        # Download manually from here:
+        # https://drive.google.com/drive/folders/0B7EVK8r0v71pTUZsaXdaSnZBZzg?
+        # resourcekey=0-rJlzl934LzC-Xp28GeIBzQ
+        celeba = dset.ImageFolder(root='data/celeba',
+                                  transform=transform3c)
+        tr_len = int(len(celeba) * 0.8)
+        te_len = len(celeba) - tr_len
+        tr_set, te_set = torch.utils.data.random_split(celeba,
+                                                       [tr_len, te_len])
+        tr_set = DataLoader(tr_set, batch_size=batch_size, shuffle=True,
+                            drop_last=True)
+        te_set = DataLoader(te_set, batch_size=batch_size, shuffle=True,
+                            drop_last=True)
     elif dataset_name == 'stl10':  # 10-class image recognition, 3 channels
         tr_set = DataLoader(
             thv.datasets.STL10(datapath, split='train', download=True,
@@ -91,4 +110,8 @@ def dataloader(batch_size, dataset_name, datapath='data'):
             thv.datasets.STL10(datapath, split='test', download=True,
                                transform=transform3c),
             batch_size=batch_size, shuffle=True, drop_last=True)
+    if dataset_name in ['mnist', 'fashion-mnist']:
+        args.in_channels = 1
+    else:
+        args.in_channels = 3
     return tr_set, te_set
